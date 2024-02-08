5eaacdc205d3653f8c921bc9314787370f2c2ff2
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-08 13:05:11 +0100

    First refactor

--- src/main/java/org/tfoc/Movie.java
+++ src/main/java/org/tfoc/Movie.java
@@ -1,29 +1,17 @@
 package org.tfoc;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class Movie {
 
     public static final int CHILDRENS = 2;
     public static final int NEW_RELEASE = 1;
     public static final int REGULAR = 0;
 
-    private String _title;
-    private int _priceCode;
-
-    public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
-    }
-
-    public int getPriceCode() {
-        return _priceCode;
-    }
-
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
-    }
-
-    public String getTitle() {
-        return _title;
-    }
+    private String title;
+    private int priceCode;
 
 }
--- src/main/java/org/tfoc/Rental.java
+++ src/main/java/org/tfoc/Rental.java
@@ -1,24 +1,17 @@
 package org.tfoc;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * The rental class represents a customer renting a movie.
  */
+
+@Builder
+@Getter
 public class Rental {
 
-    private Movie _movie;
-    private int _daysRented;
-
-    public Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
-    }
-
-    public int getDaysRented() {
-        return _daysRented;
-    }
-
-    public Movie getMovie() {
-        return _movie;
-    }
+    private Movie movie;
+    private int daysRented;
 
 }
